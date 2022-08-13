8b81a3c252e9ec5f853798914fe2e66bad61fd7d
Abbos <abbosvaliev2003@gmail.com>
2022-08-14 00:55:32 +0500

    feat: new project

---
+++ build/js/scripts.js
@@ -0,0 +1,79 @@
+// Custom Scripts
+// Custom scripts
+
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  menu.addEventListener("click", (event) => {
+    if(event.target){
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+  const nav = document.querySelector('nav')
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 1
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav')
+  } else {
+    nav.classList.remove('fixed__nav')
+  }
+}
+window.addEventListener('scroll', fixedNav)
+
+
+
+// SWIPER
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+
+    // Responsive breakpoints
+    breakpoints: {
+    // when window width is >= 320px
+    720: {
+        slidesPerView: 3,
+        spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+        slidesPerView: 2,
+        spaceBetween: 20
+    },
+    310: {
+        slidesPerView: 1,
+        spaceBetween: 10
+    },
+    }
+});
